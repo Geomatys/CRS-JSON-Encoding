4e54231ae012ce0d6119c5ac787f97f10fc53b3e
Martin Desruisseaux <martin.desruisseaux@geomatys.com>
2024-12-09 18:55:24 +0100

    First draft of a few tests.

---
+++ prototype/src/test/java/com/geomatys/json/referencing/WriteWithSIS.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership. You may not use this
+ * file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.geomatys.json.referencing;
+
+import org.apache.sis.referencing.CRS;
+import org.opengis.referencing.crs.GeodeticCRS;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Tests writing a few JSON objects from CRS definitions provided by Apache SIS.
+ */
+public class WriteWithSIS {
+    /**
+     * The writer to use for testing purpose.
+     */
+    private final Writer writer;
+
+    /**
+     * Creates a new test case.
+     */
+    public WriteWithSIS() {
+        writer = new Writer();
+    }
+
+    @Test
+    public void testGeographicCRS() throws Exception {
+        var crs = (GeodeticCRS) CRS.forCode("EPSG:4326");
+        System.out.println(writer.write(crs));
+    }
+}
